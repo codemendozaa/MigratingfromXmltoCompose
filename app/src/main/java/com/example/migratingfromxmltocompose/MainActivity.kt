71943a39098df9962c35f9b6d0ba35c3be88dcad
erixjavier <Mendhoza123#>
2023-03-08 13:20:40 -0500

    agregar imagenes y fuentes para el proyecto splash y appbar

--- app/src/main/java/com/example/migratingfromxmltocompose/MainActivity.kt
+++ app/src/main/java/com/example/migratingfromxmltocompose/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.migratingfromxmltocompose
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.migratingfromxmltocompose.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        installSplashScreen()
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            goHome()
+        }
+
+    }
+
+
+    fun goHome() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
