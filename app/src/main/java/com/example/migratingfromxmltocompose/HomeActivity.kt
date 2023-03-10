864bb8b47caff80a04d7b2e5e3745ed7f266aff3
Esteban Higuita <ehiguita@carnival.com>
2023-03-09 19:20:37 -0500

    Corregir error de compilación

--- app/src/main/java/com/example/migratingfromxmltocompose/HomeActivity.kt
+++ app/src/main/java/com/example/migratingfromxmltocompose/HomeActivity.kt
@@ -1,20 +1,21 @@
 package com.example.migratingfromxmltocompose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.migratingfromxmltocompose.databinding.ActivityHomeBinding
 import com.example.migratingfromxmltocompose.databinding.FragmentHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
-    private val binding = ActivityHomeBinding.inflate(layoutInflater)
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportFragmentManager
@@ -25,14 +26,15 @@ class HomeActivity : AppCompatActivity() {
     }
 }
 
-class HomeFragment : Fragment(){
+class HomeFragment : Fragment() {
 
-    private val binding = FragmentHomeBinding.inflate(layoutInflater)
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
         return binding.root
     }
 }
