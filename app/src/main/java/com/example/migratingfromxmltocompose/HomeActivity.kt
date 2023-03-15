e0ac07909e8e039d43bd69a59951792ddc9797ec
Esteban Higuita <ehiguita@carnival.com>
2023-03-15 13:26:11 -0500

    Crear nuevo archivo para home fragment

--- app/src/main/java/com/example/migratingfromxmltocompose/HomeActivity.kt
+++ app/src/main/java/com/example/migratingfromxmltocompose/HomeActivity.kt
@@ -1,13 +1,8 @@
 package com.example.migratingfromxmltocompose
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.example.migratingfromxmltocompose.databinding.ActivityHomeBinding
-import com.example.migratingfromxmltocompose.databinding.FragmentHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
@@ -23,18 +18,5 @@ class HomeActivity : AppCompatActivity() {
             .add(binding.fragmentContainerView.id, HomeFragment(), "home_fragment")
             .commit()
 
-    }
-}
-
-class HomeFragment : Fragment() {
-
-    private lateinit var binding: FragmentHomeBinding
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentHomeBinding.inflate(layoutInflater)
-        return binding.root
     }
 }
---
+++ app/src/main/java/com/example/migratingfromxmltocompose/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.example.migratingfromxmltocompose
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.migratingfromxmltocompose.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
