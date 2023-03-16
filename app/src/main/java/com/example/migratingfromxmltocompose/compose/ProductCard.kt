b525f5a2203b3cf46ad8d587e7474eeddd0ad931
erixjavier <Mendhoza123#>
2023-03-16 17:56:22 -0500

    correcion de card con constraint

--- app/src/main/java/com/example/migratingfromxmltocompose/compose/ProductCard.kt
+++ app/src/main/java/com/example/migratingfromxmltocompose/compose/ProductCard.kt
@@ -1,4 +1,3 @@
-@file:OptIn(ExperimentalComposeUiApi::class)
 package com.example.migratingfromxmltocompose.compose
 
 import androidx.compose.foundation.Image
@@ -18,10 +17,8 @@ import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
